ab0fdc6915feeb2d97a2e765a74dd8efe845c7ee
Jeff King <goan15910@cml23.csie.ntu.edu.tw>
2016-10-08 14:14:20 +0800

    Modify make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -6,7 +6,7 @@ CUDA_PATH=/usr/local/cuda/
 cd roi_pooling_layer
 
 nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52 -D_MWAITXINTRIN_H_INCLUDED
+	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_37 -D_MWAITXINTRIN_H_INCLUDED
 
 g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
 	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64 -D_GLIBCXX_USE_CXX11_ABI=0
