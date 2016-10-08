fdac276926187c0c9a3dfb14a0af6cebd26d2f7f
Jeff King <goan15910@cml16.csie.ntu.edu.tw>
2016-10-08 14:25:42 +0800

    Modify faster_rcnn_end2end.sh

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Usage:
-# ./experiments/scripts/faster_rcnn_end2end.sh GPU NET DATASET [options args to {train,test}_net.py]
-# DATASET is either pascal_voc or coco.
+# ./experiments/scripts/faster_rcnn_end2end.sh GPU DATASET [options args to {train,test}_net.py]
+# DATASET is either pascal_voc / coco / imagenet
 #
 # Example:
-# ./experiments/scripts/faster_rcnn_end2end.sh 0 VGG_CNN_M_1024 pascal_voc \
+# ./experiments/scripts/faster_rcnn_end2end.sh 0 imagenet \
 #   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
 
 set -x
@@ -13,9 +13,8 @@ set -e
 export PYTHONUNBUFFERED="True"
 
 GPU_ID=$1
-NET=$2
+DATASET=$2
 NET_lc=${NET,,}
-DATASET=$3
 
 array=( $@ )
 len=${#array[@]}
