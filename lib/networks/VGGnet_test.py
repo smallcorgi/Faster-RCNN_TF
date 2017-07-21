c9251e6ac621bc533a1cb268f88c194418215150
Ahmad Qamar <atqamar@gmail.com>
2017-07-21 14:16:56 -0400

    Add functionality for autoencoder (PCA) layer; not tested

--- lib/networks/VGGnet_test.py
+++ lib/networks/VGGnet_test.py
@@ -6,22 +6,23 @@ _feat_stride = [16,]
 anchor_scales = [8, 16, 32] 
 
 class VGGnet_test(Network):
-    def __init__(self, trainable=True):
+    def __init__(self, size_ae=0, trainable=True):
         self.inputs = []
         self.data = tf.placeholder(tf.float32, shape=[None, None, None, 3])
         self.im_info = tf.placeholder(tf.float32, shape=[None, 3])
         self.keep_prob = tf.placeholder(tf.float32)
         self.layers = dict({'data':self.data, 'im_info':self.im_info})
         self.trainable = trainable
+        self.size_ae = size_ae
         self.setup()
 
     def setup(self):
         (self.feed('data')
-             .conv(3, 3, 64, 1, 1, name='conv1_1', trainable=False)
-             .conv(3, 3, 64, 1, 1, name='conv1_2', trainable=False)
+             .conv(3, 3, 64, 1, 1, name='conv1_1', trainable=True)
+             .conv(3, 3, 64, 1, 1, name='conv1_2', trainable=True)
              .max_pool(2, 2, 2, 2, padding='VALID', name='pool1')
-             .conv(3, 3, 128, 1, 1, name='conv2_1', trainable=False)
-             .conv(3, 3, 128, 1, 1, name='conv2_2', trainable=False)
+             .conv(3, 3, 128, 1, 1, name='conv2_1', trainable=True)
+             .conv(3, 3, 128, 1, 1, name='conv2_2', trainable=True)
              .max_pool(2, 2, 2, 2, padding='VALID', name='pool2')
              .conv(3, 3, 256, 1, 1, name='conv3_1')
              .conv(3, 3, 256, 1, 1, name='conv3_2')
@@ -51,13 +52,22 @@ class VGGnet_test(Network):
 
         (self.feed('rpn_cls_prob_reshape','rpn_bbox_pred','im_info')
              .proposal_layer(_feat_stride, anchor_scales, 'TEST', name = 'rois'))
-        
+
         (self.feed('conv5_3', 'rois')
              .roi_pool(7, 7, 1.0/16, name='pool_5')
-             .fc(4096, name='fc6')
-             .fc(4096, name='fc7')
-             .fc(n_classes, relu=False, name='cls_score')
-             .softmax(name='cls_prob'))
+             .fc(4096, name='fc6'))
+
+        if self.size_ae == 0:
+            (self.feed('fc6')
+                 .fc(4096, name='fc7')
+                 .fc(n_classes, relu=False, name='cls_score')
+                 .softmax(name='cls_prob'))
+        else:
+            (self.feed('fc6')
+                 .fc(self.size_ae, name='fc6_ae')
+                 .fc(4096, name='fc7')
+                 .fc(n_classes, relu=False, name='cls_score')
+                 .softmax(name='cls_prob'))
 
         (self.feed('fc7')
              .fc(n_classes*4, relu=False, name='bbox_pred'))
--- lib/networks/factory.py
+++ lib/networks/factory.py
@@ -19,15 +19,15 @@ import tensorflow as tf
 #__sets['VGGnet_test'] = networks.VGGnet_test()
 
 
-def get_network(name):
+def get_network(name, size_ae=0):
     """Get a network by name."""
     #if not __sets.has_key(name):
     #    raise KeyError('Unknown dataset: {}'.format(name))
     #return __sets[name]
     if name.split('_')[1] == 'test':
-       return networks.VGGnet_test()
+       return networks.VGGnet_test(size_ae)
     elif name.split('_')[1] == 'train':
-       return networks.VGGnet_train()
+       return networks.VGGnet_train(size_ae)
     else:
        raise KeyError('Unknown dataset: {}'.format(name))
     
