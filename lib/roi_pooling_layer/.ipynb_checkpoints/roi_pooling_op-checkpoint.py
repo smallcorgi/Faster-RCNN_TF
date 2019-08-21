fcc17dbcb3dfecf72ecdbb5334e88f4b568af6f2
chaihahaha <836275709@qq.com>
2019-08-21 22:30:05 +0800

    python3

--- lib/roi_pooling_layer/.ipynb_checkpoints/roi_pooling_op-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import os.path as osp
-
-filename = osp.join(osp.dirname(__file__), 'roi_pooling.so')
-_roi_pooling_module = tf.load_op_library(filename)
-roi_pool = _roi_pooling_module.roi_pool
-roi_pool_grad = _roi_pooling_module.roi_pool_grad
